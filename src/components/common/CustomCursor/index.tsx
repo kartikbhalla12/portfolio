1cd46090611a8ef615fda56a0a18d1ee4d31d6de
Kartik Bhalla <kartikbhalla12@gmail.com>
2022-03-03 23:45:11 +0530

    src: optimize custom cursor
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/components/common/CustomCursor/index.tsx
+++ src/components/common/CustomCursor/index.tsx
@@ -1,33 +1,67 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, RefObject, useEffect, useRef, useState } from 'react';
+import classnames from 'classnames';
 
 import styles from './customCursor.module.scss';
 
 const CustomCursor: FC = () => {
-	const [top, setTop] = useState(-50);
-	const [left, setLeft] = useState(-50);
 	const [isLink, setIsLink] = useState(false);
+	const [hideCursor, setHideCursor] = useState(false);
+
+	const cursorDot = useRef<HTMLDivElement>(null);
+	const cursorRing = useRef<HTMLDivElement>(null);
+
+	const getElementTagName = (e: MouseEvent) => {
+		return (e.target as HTMLElement).tagName.toLowerCase();
+	};
+
+	const setCursorCoordinates = (
+		element: RefObject<HTMLElement>,
+		left: number,
+		top: number
+	) => {
+		element.current!.style.top = `${top}px`;
+		element.current!.style.left = `${left}px`;
+	};
 
 	const handleMouseMove = (e: MouseEvent) => {
-		setLeft(e.clientX);
-		setTop(e.clientY);
+		setCursorCoordinates(cursorDot, e.clientX, e.clientY);
+		setCursorCoordinates(cursorRing, e.clientX, e.clientY);
 
 		const elementsList = ['a', 'button', 'img'];
-		const tagName = (e.target as HTMLElement).tagName;
+		const tagName = getElementTagName(e);
 
-		if (tagName) setIsLink(elementsList.includes(tagName.toLowerCase()));
+		if (tagName) setIsLink(elementsList.includes(tagName));
+	};
+
+	const handleMouseLeave = (e: MouseEvent) => {
+		if (getElementTagName(e) == 'html') setHideCursor(true);
+	};
+
+	const handleMouseEnter = (e: MouseEvent) => {
+		if (getElementTagName(e) == 'html') setHideCursor(false);
 	};
 
 	useEffect(() => {
 		window.addEventListener('mousemove', handleMouseMove);
-		return () => window.removeEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseleave', handleMouseLeave, true);
+		window.addEventListener('mouseenter', handleMouseEnter, true);
+
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseleave', handleMouseLeave, true);
+			window.removeEventListener('mouseenter', handleMouseEnter, true);
+		};
 	}, []);
 
-	const cursorPosition = { top, left };
-
 	return (
-		<div className={`${styles.customCursor} ${isLink ? styles.link : ''}`}>
-			<div style={cursorPosition} className={styles.cursorDot} />
-			<div style={cursorPosition} className={styles.cursorRing} />
+		<div
+			className={classnames(
+				styles.customCursor,
+				{ [styles.link]: isLink },
+				{ [styles.hidden]: hideCursor }
+			)}>
+			<div ref={cursorDot} className={styles.cursorDot} />
+			<div ref={cursorRing} className={styles.cursorRing} />
 		</div>
 	);
 };
