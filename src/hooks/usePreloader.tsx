3ee1e1797353065faaeb62f854f15cca27329329
Kartik Bhalla <kartikbhalla12@gmail.com>
2022-03-06 03:53:58 +0530

    src: hooks: fix initial section scroll due to preloader
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/hooks/usePreloader.tsx
+++ src/hooks/usePreloader.tsx
@@ -1,11 +1,17 @@
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const usePreloader = () => {
 	const [loading, setLoading] = useState(true);
+	const router = useRouter();
 
 	useEffect(() => {
 		setTimeout(() => setLoading(false), 3000);
 	});
+
+	useEffect(() => {
+		if (router.asPath.split('/#')[1] && !loading) router.replace(router.asPath);
+	}, [loading]);
 
 	return {
 		loading,
