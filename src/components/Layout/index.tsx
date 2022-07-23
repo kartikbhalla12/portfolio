5e01a71cae6f6ecb3660278c95e854b4f50a43ab
Kartik Bhalla <kartikbhalla12@gmail.com>
2022-07-23 18:00:58 +0530

    Layout: add meta information
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import classNames from 'classnames';
+import Head from 'next/head';
 
 import Footer from '@components/common/Footer';
 import Navbar from '@components/common/Navbar';
@@ -19,9 +20,69 @@ const Layout: FC<LayoutProps> = ({ children, isMobile }) => {
 
 	return (
 		<>
+			<Head>
+				<title>Kartik Bhalla</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta name='author' content='Kartik Bhalla' />
+				<meta charSet='utf-8' />
+				<meta
+					name='keywords'
+					content="
+						kartik Bhalla, 
+						kartik bhalla dev, 
+						kartik bhalla, 
+						kartik dev, 
+						bhalla dev, 
+						Kartik, 
+						Bhalla, 
+						thedemon12, 
+						kartikbhalla, 
+						kartikbhalla.dev, 
+						thedemon, 
+						kartik's, 
+						kartik bhalla's, 
+						kartik bhalla's portfolio, 
+						kartik's portfolio, 
+						Kartik Bhalla Website, 
+						upgrad,
+						upgrad abroad,
+						software developer upgrad,
+						software developer,
+						noida, 
+						study abroad,
+						technology,
+						nsut, 
+						netaji subhas university of technology, 
+						netaji subhas university of technology east campus, 
+						netaji subhas,
+						aiactr,
+						ait,
+						ambedkar institute,
+						ambedkar institute of advanced communication technologies and research,
+						nsut east campus, 
+						east, 
+						east campus, 
+						training and placement,
+						placement coordinator,
+						tnp,
+						training, 
+						placement, 
+						react,
+						reactjs,
+						next,
+						nextjs,
+						New Delhi, 
+						MERN Stack, 
+						Full Stack Dev, 
+						Full Stack Developer, 
+						Cloud"
+				/>
+				<link rel='icon' href='/logo.svg' />
+			</Head>
 			<Preloader isMobile={isMobile} loading={loading} />
 			<Navbar isMobile={isMobile} theme={theme} onThemeChange={setTheme} />
 			<div
+				id='layout'
 				className={classNames(styles.layout, {
 					[styles.preloader]: loading,
 					[styles.mobile]: isMobile,
