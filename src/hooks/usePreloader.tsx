6c60a20e7d5775e3433c26b59bf0134f159f0a73
Kartik Bhalla <kartikbhalla12@gmail.com>
2022-08-01 21:53:08 +0530

    hooks: fix page not loading on certain devices
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/hooks/usePreloader.tsx
+++ src/hooks/usePreloader.tsx
@@ -2,19 +2,13 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const usePreloader = () => {
-	const [timerOut, setTimerOut] = useState(false);
-	const [pageLoaded, setPageLoaded] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const router = useRouter();
 
 	useEffect(() => {
-		setTimeout(() => setTimerOut(true), 2000);
+		setTimeout(() => setLoading(false), 3000);
 	}, []);
 
-	useEffect(() => {
-		window.onload = () => setPageLoaded(true);
-	}, []);
-
-	let loading = !(timerOut && pageLoaded);
 	useEffect(() => {
 		if (router.asPath.split('/#')[1] && !loading) router.replace(router.asPath);
 	}, [loading]);
